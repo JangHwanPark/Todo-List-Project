fe48ac3f041ca4250618dc44146627bf089f7e8c
JangHwanPark <wkd5878@gmail.com>
2023-03-04 21:02:56 +0900

    feat : 작업 추가 기능 추가

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
 /* 재료 */
+const inputTodoText = document.querySelector('.input-todo');    // input 태그
+const btnTodoSubmit = document.querySelector('.btn-todo');      // button 태그 (input 자식)
+const menuTodoBox = document.querySelector('.add-todo-list');   // 추가될 list 테두리
 
-// input 태그
-const inputTodoText = document.querySelector('.input-todo');
+const arrLocalStorage = [];     // 리스트를 로컬 스토리지에 추가할 빈 배열
 
-// button 태그 (input 자식)
-const btnTodoSubmit = document.querySelector('.btn-todo');
 
-// 추가될 list 테두리
-const menuTodoBox = document.querySelector('.add-todo-list');
-
-// 리스트를 로컬 스토리지에 추가할 빈 배열
-const arrLocalStorage = [];
-
-/* 요소 생성 & 요소에 클래스 추가 */
-function onAddElementClass() {
+/* 요소 생성 & 요소에 클래스, 텍스트 추가 & 노드 연결 */
+function onAddBtnOnClick(event) {
+    event.preventDefault()  // 서밋시 새로고침 방지
     const todoListWrapper = document.createElement('li');
     const todoSpanTextValue = document.createElement('span');
     const todoButtonDeleteList = document.createElement('button');
 
-    todoListWrapper.className = 'todo-buttond-elete-list';
+    todoListWrapper.className = 'todo-button-element-list';
     todoSpanTextValue.className ='input-todo-value';
     todoButtonDeleteList.className = 'list-delete';
-}
 
+    todoSpanTextValue.textContent = inputTodoText.value;
+    todoButtonDeleteList.textContent = 'X';
 
-/* 리스트 추가 기능 */
-function onAddBtnOnClick(event) {
-    event.preventDefault()  // 서밋시 새로고침 방지
-    menuTodoBox.insertAdjacentHTML('beforeend', );
+    menuTodoBox.append(todoListWrapper);
+    todoListWrapper.append(todoSpanTextValue);
+    todoListWrapper.append(todoButtonDeleteList);
 }
 
 
 /* 로컬 스토리지 추가 기능 */
 function onAddListLocalStorage() {
-
+    localStorage.setItem('Todo', JSON.stringify(arrLocalStorage));
 }
 
 
 /* 작업 완료 버튼 기능 */
 function delTodoListClear() {
-    
+    const endButtonTodo = document.querySelector('.list-delete');
+    const delButtonTodo = document.querySelector('.todo-button-element-list');
 }
 
 
